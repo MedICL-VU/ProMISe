74649e068988d27ab67a31b7d84e3b42aa31c80c
HaoLi12345 <56284140+HaoLi12345@users.noreply.github.com>
2023-11-11 22:58:51 -0600

    Add files via upload

--- promise/src/config/config_setup.py
+++ promise/src/config/config_setup.py
@@ -10,23 +10,23 @@ from segment_anything import SamAutomaticMaskGenerator, sam_model_registry
 from torch.optim import AdamW
 from monai.losses import DiceCELoss, DiceLoss
 def load_data_set(args, split=''):
-    if args.split == 'train':
+    if split == 'train':
         data = load_data_volume(
             data=args.data,
             data_dir=args.data_dir,
             batch_size=args.batch_size,
             augmentation=True,
-            split="train",
+            split=split,
             rand_crop_spatial_size=args.rand_crop_size,
             num_worker=args.num_worker
         )
-    elif args.split == 'val':
+    elif split == 'val':
         data = load_data_volume(
             data=args.data,
             data_dir=args.data_dir,
             batch_size=1,
             augmentation=False,
-            split="val",
+            split=split,
             deterministic=True,
             rand_crop_spatial_size=args.rand_crop_size,
             num_worker=args.num_worker
@@ -37,7 +37,7 @@ def load_data_set(args, split=''):
         batch_size=1,
         data_dir=args.data_dir,
         augmentation=False,
-        split="test",
+        split=split,
         rand_crop_spatial_size=args.rand_crop_size,
         convert_to_sam=False,
         do_test_crop=False,
